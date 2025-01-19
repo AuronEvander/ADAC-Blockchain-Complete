e0df246ecad1b9ad66aaf3114e9d90d9fa376ca0
Auron Evander <31187773+AuronEvander@users.noreply.github.com>
2025-01-19 20:28:45 +0300

    Add proper Python package configuration

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="adac-blockchain",
     version="0.1.0",
-    author="ADAC Team",
-    description="ADAC Blockchain Implementation",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/BurcUnalan/ADAC-Blockchain-Complete",
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    install_requires=[
+        'fastapi',
+        'uvicorn',
+        'sqlalchemy',
+        'alembic',
+        'psycopg2-binary',
     ],
-    python_requires=">=3.9",
+    python_requires='>=3.8',
 )
