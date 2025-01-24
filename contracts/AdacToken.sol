5314b7aed6c520402f87be60e95666bd436f5bc3
Auron Evander <31187773+AuronEvander@users.noreply.github.com>
2025-01-24 19:49:09 +0100

    Add AdacToken contract

---
+++ contracts/AdacToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AdacToken is ERC20, Ownable {
+    uint256 private constant INITIAL_SUPPLY = 100_000_000 * 10**18; // 100M tokens
+    
+    constructor() ERC20("ADAC Token", "ADAC") Ownable(msg.sender) {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
